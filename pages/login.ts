8a99e7298fb6d475d11e656cee72376f54b7b1c5
Jaime <jaimegenerali@gmail.com>
2023-03-07 12:04:12 -0300

    added test about not including username

--- pages/login.ts
+++ pages/login.ts
@@ -8,6 +8,7 @@ export class login {
     readonly passwordRegister: Locator;
     readonly btnRegister: Locator;
     readonly logoutMsg: Locator;
+    readonly errorUsernameMsg: Locator;
 
     constructor (page: Page) {
         this.page;
@@ -17,7 +18,8 @@ export class login {
         this.emailAddressRegister = page.locator('#reg_email');
         this.passwordRegister = page.locator('#reg_password');
         this.btnRegister = page.getByRole('button', { name: 'Register' });
-        this.logoutMsg = page.getByRole('link', { name: 'Log out' })
+        this.logoutMsg = page.getByRole('link', { name: 'Log out' });
+        this.errorUsernameMsg = page.getByText('Error: Please enter a valid account username.')
     }
     async myAccountt () {
         await this.myAccount.click()
@@ -31,6 +33,12 @@ export class login {
         await this.passwordRegister.fill(password)
         await this.btnRegister.click()
         await expect(this.logoutMsg).toBeVisible()
+    }
+    async fillingEmailPassword(email: string, password: string) {
+        await this.emailAddressRegister.fill(email)
+        await this.passwordRegister.fill(password)
+        await this.btnRegister.click() 
+        await expect(this.errorUsernameMsg).toBeVisible()
     }
     
 }
--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -19,7 +19,17 @@ test.describe('Testes de criação e login de usuários', () => {
         await Login.registerWord();
         //filling username,email, and password
         await Login.fillingDatas(userName, emailAddress, passwordRe)
-        
+     });
+    test.only('verificando se é possível cadastrar um usuário sem preencher username', async ({ page }) => {
+        const Login = new login(page);
+        const url = '/my-account/'
+        //clicking im my Account link
+        await Login.myAccountt();
+        await expect(page).toHaveURL(url);
+        //waiting for the display of the h2 Register
+        await Login.registerWord(); 
+        // filling email, and password
+        await Login.fillingEmailPassword(emailAddress, passwordRe)
     });
     
     
